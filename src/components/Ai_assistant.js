941edba6f1fd4473818e8e46381f87496a536af9
dauqu <sujeetsingh734321@gmail.com>
2023-04-15 15:31:54 +0530

    ch1504231531

--- src/components/Ai_assistant.js
+++ src/components/Ai_assistant.js
@@ -6,8 +6,8 @@ export default function Ai_assistant() {
     <div className='px-4 pt-[80px]'>
         <div className='max-w-[1250px] m-auto'>
             <p className='text-[14px] leading-[21px] text-[#BB66CC] font-bold text-center'>AI ASSISTANT</p>
-            <p className='text-[20px] leading-[30px] sm:text-[30px] sm:leading-[40px] text-[#0a001f] font-extrabold text-center mt-6'>SCRIBBLER IS JUST LIKE WRITING EFFORTLESSLY BUT ON STEROIDS</p>
-            <p className='text-[38px] leading-[46px] sm:text-[46px] sm:leading-[51px] text-[#0a001f] font-extrabold text-center mt-6'>Generate months of social media content in minutes</p>
+            <p className='text-[20px] leading-[30px] sm:text-[30px] sm:leading-[40px] text-[#0a001f] font-extrabold text-center mt-6 max-w-[900px] m-auto'>SCRIBBLER IS JUST LIKE WRITING EFFORTLESSLY BUT ON STEROIDS</p>
+            <p className='text-[38px] leading-[46px] sm:text-[46px] sm:leading-[51px] text-[#0a001f] font-extrabold text-center mt-6 max-w-[700px] m-auto'>Generate months of social media content in minutes</p>
             <div className='hidden justify-center'>
                 <p className='text-[#555] text-[16px] leading-[22px] sm:text-[22px] sm:leading-[31px] text-center mt-3'>Generate months of social media content in minutes</p>
             </div>
